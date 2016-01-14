6efee360d5be0662467dcecd2f889e5a0f589c98
Boris Xiao <borishaw@gmail.com>
2016-01-13 19:27:15 -0500

    jQuery Validation

--- form.js
+++ form.js
@@ -11,18 +11,46 @@ $('window').ready(function () {
         $(".jquery-response p").html('Fail to Send, please try again');
     });
 
-    //Send form with AJAX
-    $('#contact').on('submit', function(e){
-        e.preventDefault();
-        var formData = JSON.stringify($('#contact').serializeArray());
-        $.post('process.php', formData, function(resp){
-            resp = $.parseJSON(resp);
-            $(".jquery-response p").html(resp.message);
-            console.log(resp);
-        });
-    });
-
     //Masked Input
     $("#phone").mask("(999) 999-9999");
 
+    //Validate Form
+    $('#contact').validate({
+        debug: true,
+
+        rules: {
+            'First Name':{
+                required: true
+            },
+            'Last Name':{
+                required: true
+            },
+            'Email': {
+                required: true,
+                email: true
+            },
+            'Phone': {
+                required: true,
+                phoneUS: true
+            },
+            'Role': {
+                required: true,
+            },
+            'Heard From': {
+                required: true
+            }
+        },
+
+        submitHandler: function(){
+            var formData = JSON.stringify($('#contact').serializeArray());
+            $.post('process.php', formData, function(resp){
+                console.log(resp);
+                resp = $.parseJSON(resp);
+                $(".jquery-response p").html(resp.message);
+                if (resp.success == 1) {
+                    $('#contact').trigger('reset');
+                }
+            });
+        }
+    });
 });
--- process.php
+++ process.php
@@ -2,6 +2,13 @@
 //Delay response for n seconds
 sleep(2);
 
+//Redirect to index.html if the request method is not POST
+if ($_SERVER['REQUEST_METHOD'] != 'POST'){
+    header('location: index.html');
+    die;
+}
+
+
 $json_response = ['success' => 0, 'message' => ''];
 
 //Throw an error
@@ -13,16 +20,23 @@ $formData = json_decode(file_get_contents('php://input'), true);
 //Format Form Data to one single array
 $formDataArray = array();
 foreach ($formData as $data){
-    $formDataArray[$data['name']] = $data['value'];
+    $formDataArray[$data['name']] = htmlspecialchars($data['value']);
 }
 $email = $formDataArray['Email'];
 $phone = $formDataArray['Phone'];
 
-//Load libraries
+//Load PHP libraries
 require_once __DIR__ . '/vendor/autoload.php';
 
-//ReCaptcha and Validtor
+//Validate User Input
 use Respect\Validation\Validator as v;
+foreach ($formDataArray as $data) {
+    if (!v::notEmpty()->validate($data)){
+        $json_response['message'] = 'Please enter all required fields';
+        die(json_encode($json_response));
+    }
+}
+
 if (!v::email()->validate($email)){
     $json_response['message'] = 'Invalid Email Address';
     die(json_encode($json_response));
@@ -33,12 +47,13 @@ if (!v::phone()->validate($phone)){
     die(json_encode($json_response));
 }
 
+//Validate Google ReCaptcha
 $recaptcha = new \ReCaptcha\ReCaptcha('6LdGKBUTAAAAABbnSImOug65r_8-dYzneRNx8mxj');
 $resp = $recaptcha->verify($formDataArray['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
 if ($resp->isSuccess()) {
     unset($formDataArray['g-recaptcha-response']);
     $mail = new PHPMailer();
-    $mail->setFrom($formDataArray['Email'], $formDataArray['Name']);
+    $mail->setFrom($formDataArray['Email'], $formDataArray['First Name'].' '.$formDataArray['Last Name']);
     $mail->addAddress('julie@ankitdesigns.com', 'Julie');
     $mail->isHTML(true);
     $mail->Subject = 'New Registration';
@@ -48,7 +63,7 @@ if ($resp->isSuccess()) {
         $mail->Body .= $key . ': ' . $value. '<br/>';
     }
     if(!$mail->send()) {
-        $json_response['message'] = 'Message could not be sent.' . ' Mailer Error: ' . $mail->ErrorInfo;
+        $json_response['message'] = 'Message could not be sent.' . ' Please try again.' . ' Mailer Error: ' . $mail->ErrorInfo;
         die(json_encode($json_response));
     } else {
         $json_response['message'] = 'Message has been sent';
@@ -57,6 +72,6 @@ if ($resp->isSuccess()) {
     }
 } else {
     $errors = $resp->getErrorCodes();
-    $json_response['message'] = 'ReCaptcha Failed. Error Code: '. print_r($errors);
+    $json_response['message'] = 'ReCaptcha Failed. Error Code: '. $errors[0];
     die(json_encode($json_response));
 }
